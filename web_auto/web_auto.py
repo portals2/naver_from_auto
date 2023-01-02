d919f2d7d303a4e18679a83a95dc4832935ec7a7
portals2 <dkfvkswotdj@naver.com>
2023-01-02 21:20:08 +0900

    ver1.1

--- web_auto/web_auto.py
+++ web_auto/web_auto.py
@@ -9,23 +9,28 @@ import time
 
 driver = webdriver.Chrome()
 
-def perf(url, n): #첫 번째 선택지만 선택
+def perf(url, n, b): #첫 번째 선택지만 선택
     driver.get(url)
 
     time.sleep(1)
 
-    for i in range(n-1):
-        driver.execute_script('window.open("%s");'%url)
-
     for i in range(n):
-        driver.switch_to.window(driver.window_handles[i])
+        driver.execute_script('window.open("%s");'%url)
+    print('{0}web_loding_done{0}'.format('@'*10))
+
+    for j in range(n):
+        driver.switch_to.window(driver.window_handles[j])
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB)
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB)
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB)
-        for i in range(n+1): # n
+        for i in range(b+1): # n
             driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB,Keys.ENTER)
-        time.sleep(1)
+        print(j+1)
+        # time.sleep(1)
 
-    while(True):pass 
+    print('{0}all_done{0}'.format('@'*10))
 
-perf('https://naver.me/FTkLTYx3', 10) # 'url', 횟수
+
+perf('https://naver.me/FTkLTYx3', 30, 10) # 'url', 횟수, 문제개수
+
+# 2, 3은 화살표 아래키 입력 램덤
